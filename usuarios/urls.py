dba592ee73128f290f47747be673ff288a82260b
Luiz Fernando Balbino <106929467+NandoLu@users.noreply.github.com>
2024-11-07 08:56:30 -0300

    Add files via upload

---
+++ usuarios/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('cadastrar_vendedor/', views.cadastrar_vendedor, name="cadastrar_vendedor")
+]
---
+++ usuarios/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+def cadastrar_vendedor(request):
+    return HttpResponse('Teste')
